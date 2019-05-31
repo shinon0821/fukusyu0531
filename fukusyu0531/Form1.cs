ca5ee8c0c6011e96f80ab99c9dc404e8890e5367
shinon0821 <49701829+shinon0821@users.noreply.github.com>
2019-05-31 13:30:56 +0900

    fukusyu0531

--- fukusyu0531/Form1.cs
+++ fukusyu0531/Form1.cs
@@ -12,9 +12,39 @@ namespace fukusyu0531
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -5;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+            if (label1.Left <= 0)
+            {
+                vx = -vx;
+            }
+            if (label1.Top <= 0)
+            {
+                vy = -vy;
+            }
+            if (label1.Left >= 470)
+            {
+                vx = -vx;
+            }
+            if (label1.Top >= 400)
+            {
+                vy = -vy;
+                vy = vy;
+            }
         }
     }
 }
